35e9f71e8afd26a29dcb8da2e125db386fc0eaac
Rolando Cruz <pspnando@me.com>
2016-12-30 23:41:45 -0800

    create linkedList.py

---
+++ linkedList.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+    def insert(self,head,data): 
+        if head == None:
+            head = Node(data)
+        else:
+            current = head
+            while current.next:
+                current = current.next
+            current.next = Node(data)
+        return head
